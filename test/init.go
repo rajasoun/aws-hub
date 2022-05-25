502402653f8e7531b125263edd84ee33d9748ac4
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-25 03:49:47 +0000

    refactor(init_test): implicit setting of flag

--- test/init.go
+++ test/init.go
@@ -2,14 +2,21 @@ package test
 
 import (
 	"flag"
+	"fmt"
 	"testing"
 )
 
+// Initialize go test
 func init() {
 	testing.Init()
+	flag.Bool("isTest", true, "Returns true if run from go test")
 	flag.Parse()
 }
 
+// Returns true if invoked with go test -v or go test
 func IsTestRun() bool {
-	return flag.Lookup("test.v").Value.(flag.Getter).Get().(bool)
+	fmt.Println()
+	verbose := flag.Lookup("test.v").Value.(flag.Getter).Get().(bool)
+	isTest := flag.Lookup("isTest").Value.(flag.Getter).Get().(bool)
+	return verbose || isTest
 }
