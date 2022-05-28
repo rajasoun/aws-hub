350de961e0a892f9c06935d11e68437e03b19636
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-28 13:50:01 +0000

    refactor(cost): enable stubing for tdd - Barebone

--- services/aws/s3/getobject_test.go
+++ services/aws/s3/getobject_test.go
@@ -12,18 +12,18 @@ import (
 
 var bucketContent = "this is the body foo bar baz"
 
-type mockGetObjectAPI func(ctx context.Context,
+type MockS3GetObjectAPI func(ctx context.Context,
 	params *s3.GetObjectInput,
 	optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 
-func (mock mockGetObjectAPI) GetObject(ctx context.Context,
+func (mock MockS3GetObjectAPI) GetObject(ctx context.Context,
 	params *s3.GetObjectInput,
 	optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
 	return mock(ctx, params, optFns...)
 }
 
-func mockS3GetObjectAPI(t *testing.T) S3GetObjectAPI {
-	return mockGetObjectAPI(func(ctx context.Context,
+func mockS3GetObjectAPI() S3GetObjectAPI {
+	return MockS3GetObjectAPI(func(ctx context.Context,
 		params *s3.GetObjectInput,
 		optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
 		result := &s3.GetObjectOutput{
@@ -40,15 +40,15 @@ func TestGetObjectFromS3(t *testing.T) {
 
 	cases := []struct {
 		name   string
-		client func(t *testing.T) S3GetObjectAPI
+		client func() S3GetObjectAPI
 		bucket string
 		key    string
 		expect []byte
 	}{
 		{
 			name: "Check Get Object From S3",
-			client: func(t *testing.T) S3GetObjectAPI {
-				return mockS3GetObjectAPI(t)
+			client: func() S3GetObjectAPI {
+				return mockS3GetObjectAPI()
 			},
 			bucket: "testBucket",
 			key:    "testKey",
@@ -58,7 +58,7 @@ func TestGetObjectFromS3(t *testing.T) {
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			content, err := GetObjectFromS3(ctx, tt.client(t), tt.bucket, tt.key)
+			content, err := GetObjectFromS3(ctx, tt.client(), tt.bucket, tt.key)
 			assert.NoError(err, "expect no error, got %v", err)
 			assert.Equal(tt.expect, content, "expect %v, got %v", tt.expect, content)
 		})
