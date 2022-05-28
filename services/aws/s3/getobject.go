9c812d04b91be0adec8dd5585c3fb941c066619e
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-28 14:01:04 +0000

    refactor(cost): enable stubing for tdd - Barebone

--- services/aws/s3/getobject.go
+++ services/aws/s3/getobject.go
@@ -7,15 +7,17 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+// Interface for Amazon S3 API operations required by GetObjectFromS3 function
 type S3GetObjectAPI interface {
 	GetObject(ctx context.Context,
 		params *s3.GetObjectInput,
 		optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 }
 
+//Amazon S3 client’s GetObject method,
 func GetObjectFromS3(ctx context.Context,
-	api S3GetObjectAPI, bucket, key string) ([]byte, error) {
-	object, err := api.GetObject(ctx, &s3.GetObjectInput{
+	client S3GetObjectAPI, bucket, key string) ([]byte, error) {
+	object, err := client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: &bucket,
 		Key:    &key,
 	})
