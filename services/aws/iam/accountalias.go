012c5722873df99f6330a7476c28df0940961d9d
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-29 05:25:29 +0000

    refactor(cost): enable stubing for tdd - Barebone

--- services/aws/iam/accountalias.go
+++ services/aws/iam/accountalias.go
@@ -11,32 +11,32 @@ type Aliases struct {
 	List []string `json:"list"`
 }
 
-// Interface wraps up the underlying AWS Function
-// This will enable TDD using mocking the wrapped function
+// Interface for Amazon IAM ListAccountAliases API
+// This will enable TDD using mocking
 type IAMListAccountAliasesAPI interface {
+	iam.ListAccountAliasesAPIClient // Only for Refernce to Actual Client
 	ListAccountAliases(ctx context.Context,
 		params *iam.ListAccountAliasesInput,
 		optFns ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error)
 }
 
+// Wrapper Function to AWS IAM ListAccountAliases API with client as argument
+// This will enable TDD by passing mock client
+func ListAccountAliases(ctx context.Context, client IAMListAccountAliasesAPI,
+	input *iam.ListAccountAliasesInput) (*iam.ListAccountAliasesOutput, error) {
+	return client.ListAccountAliases(ctx, input)
+}
+
 // GetAccountAliases retrieves the aliases for your AWS Identity and Access Management (IAM) account.
 // Inputs:
-//     c is the context of the method call, which includes the AWS Region.
-//     api is the interface that defines the method call.
-//     input defines the input arguments to the service call.
+//     client is iam.NewFromConfig(cfg) & cfg is the context of the method call
 // Output:
 //     If successful, a ListAccountAliasesOutput object containing the result of the service call and nil.
 //     Otherwise, nil and an error from the call to ListAccountAliases.
-func GetAccountAliases(c context.Context, client IAMListAccountAliasesAPI,
-	input *iam.ListAccountAliasesInput) (*iam.ListAccountAliasesOutput, error) {
-	return client.ListAccountAliases(c, input)
-
-}
-
 func GetAliases(client IAMListAccountAliasesAPI) (Aliases, error) {
 	var ctx context.Context = context.TODO()
 	input := &iam.ListAccountAliasesInput{}
-	result, err := GetAccountAliases(ctx, client, input)
+	result, err := ListAccountAliases(ctx, client, input)
 	if err != nil {
 		log.Println("Got an error retrieving account aliases")
 		return Aliases{List: []string{}}, err
--- services/aws/iam/accountalias_test.go
+++ services/aws/iam/accountalias_test.go
@@ -9,38 +9,58 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//type ListAccountAliasesImpl struct{}
+var testAlias string = "aws-test-account-alias"
 
-type mockListAccountAliases struct{}
+//Mock Function
+type MockIAMListAccountAliasesAPI func(ctx context.Context,
+	params *iam.ListAccountAliasesInput,
+	optFns ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error)
 
-func (dt mockListAccountAliases) ListAccountAliases(ctx context.Context,
+// AWS IAM ListAccountAliases Method with Mock Function Receiver
+func (mock MockIAMListAccountAliasesAPI) ListAccountAliases(ctx context.Context,
 	params *iam.ListAccountAliasesInput,
 	optFns ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
-	aliases := []string{
-		"aws-docs-account-alias1",
-		"aws-docs-account-alias2",
-	}
-	output := &iam.ListAccountAliasesOutput{
-		AccountAliases: aliases,
-	}
-	return output, nil
+	return mock(ctx, params, optFns...)
 }
 
-func TestGetAccountAliases(t *testing.T) {
-	t.Run("Check GetAccountAliases", func(t *testing.T) {
-		assert := assert.New(t)
-		client := &mockListAccountAliases{}
-		input := &iam.ListAccountAliasesInput{}
-		want := 2
-		got, err := GetAccountAliases(context.Background(), client, input)
-		assert.NoError(err, "err = %v, want = nil", err)
-		assert.Equal(want, len(got.AccountAliases), "got = %v , want = %v", got, want)
+func mockIAMListAccountAliasesAPIOutput() IAMListAccountAliasesAPI {
+	return MockIAMListAccountAliasesAPI(func(ctx context.Context,
+		params *iam.ListAccountAliasesInput,
+		optFns ...func(*iam.Options)) (*iam.ListAccountAliasesOutput, error) {
+		aliases := []string{testAlias}
+		result := &iam.ListAccountAliasesOutput{
+			AccountAliases: aliases,
+		}
+		return result, nil
 	})
 }
 
 func TestGetAliases(t *testing.T) {
-	t.Run("Check GetAliases returns err with aws.Config{}", func(t *testing.T) {
-		assert := assert.New(t)
+	assert := assert.New(t)
+	t.Parallel()
+
+	cases := []struct {
+		name   string
+		client func() IAMListAccountAliasesAPI
+		want   string
+	}{
+		{
+			name: "Check Get Aliases",
+			client: func() IAMListAccountAliasesAPI {
+				return mockIAMListAccountAliasesAPIOutput()
+			},
+			want: testAlias,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAliases(tt.client())
+			assert.NoError(err, "expect no error, got %v", err)
+			assert.Equal(tt.want, got.List[0], "got GetUserCount = %v, want = %v", got.List[0], tt.want)
+		})
+	}
+	t.Run("Check GetUserCount returns err with Empty aws.Config{}", func(t *testing.T) {
 		emptyCfg := aws.Config{}
 		noOpClient := iam.NewFromConfig(emptyCfg)
 		_, err := GetAliases(noOpClient)
