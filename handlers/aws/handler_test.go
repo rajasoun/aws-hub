e5e9bdf52680b4c971ed5e8d45028652922b2738
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-30 10:42:30 +0000

    refactor(iam): simplify mocking logic

--- handlers/aws/handler_test.go
+++ handlers/aws/handler_test.go
@@ -11,28 +11,25 @@ import (
 )
 
 func TestIAMHandlers(t *testing.T) {
-	if os.Getenv("SKIP_E2E") != "" {
-		t.Skip("Skipping INTEGRATION Tests")
-	}
+	t.Parallel()
 	tests := []struct {
 		name               string
 		endPoint           string
 		hasMultipleProfile bool
 		wantKey            string
 	}{
-		{"Check IAMGetAliasesHandler", "/aws/iam/alias", false, "list"},
-		{"Check IAMGetUserIdentityHandler", "/aws/iam/account", false, "username"},
-		{"Check IAMGetUserCountHandler", "/aws/iam/users", false, "usercount"},
+		{"Check ConfigProfilesHandler", "/aws/profiles", false, "multiple"},
+		{"Check ConfigProfilesHandler", "/aws/profiles", true, "multiple"},
+		{"Check HealthCheckHandler", "/health", false, "http-server-alive"},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			handler := NewDefaultAWSHandler(tt.hasMultipleProfile)
 			router := handler.SetUpRoutes()
 			server := httptest.NewServer(handlers.LoggingHandler(os.Stdout, router))
 			defer server.Close()
-
 			expect := httpexpect.New(t, server.URL)
-
 			expect.GET(tt.endPoint).
 				Expect().
 				Status(http.StatusOK).
