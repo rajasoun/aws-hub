e9cdcd437a6e3e4453ba2f31443026df905259cb
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-30 08:22:04 +0000

    refactor(iam): simplify mocking logic

--- handlers/aws/handler.go
+++ handlers/aws/handler.go
@@ -48,7 +48,7 @@ func (handler *AWSHandler) API(r *http.Request, w http.ResponseWriter,
 	if foundInCache {
 		respondWithJSON(w, http.StatusOK, response)
 	} else {
-		response, err := handler.aws.ExternalServiceGateway(cfg, apiName)
+		response, err := handler.ExternalServiceGateway(cfg, apiName)
 		handler.respondWithJSONandSetCache(response, err, w, errMsg, key)
 	}
 }
--- handlers/aws/route_handler_api.go
+++ handlers/aws/route_handler_api.go
@@ -1,7 +1,11 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/gorilla/mux"
+
+	hubIAM "github.com/rajasoun/aws-hub/services/aws/iam"
 )
 
 func (handler *AWSHandler) SetUpRoutes() *mux.Router {
@@ -16,4 +20,21 @@ func (handler *AWSHandler) SetUpRoutes() *mux.Router {
 	router.HandleFunc("/aws/cost/instance_type", handler.CostAndUsagePerInstanceTypeHandler)
 	router.HandleFunc("/health", handler.HealthCheckHandler)
 	return router
+}
+
+// ToDo Technical Debt - Use Interface to call the right method
+// Use Dependency Injection
+func (handler *AWSHandler) ExternalServiceGateway(cfg aws.Config, apiName string) (interface{}, error) {
+	var response interface{}
+	var err error
+	var client *iam.Client = iam.NewFromConfig(cfg)
+	switch {
+	case apiName == "GetUserCount":
+		response, err = hubIAM.GetUserCount(client)
+	case apiName == "GetUserIdentity":
+		response, err = hubIAM.GetUserIdentity(client)
+	case apiName == "GetAliases":
+		response, err = hubIAM.GetAliases(client)
+	}
+	return response, err
 }
