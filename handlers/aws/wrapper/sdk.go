d326013e5c07d6cc8daa56e0c0f6ced4ca4613d8
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-02 19:25:41 +0000

    refactor(mock): aws sdk mocking

--- handlers/aws/wrapper/sdk.go
+++ handlers/aws/wrapper/sdk.go
@@ -6,35 +6,35 @@ import (
 )
 
 type Account struct {
-	client apiclient.IAMListAccountAliasesAPIClient
-	alias  hubIAM.Aliases
+	client   apiclient.IAMListAccountAliasesAPIClient
+	response hubIAM.Aliases
 }
 
 func (account *Account) Execute() error {
 	response, err := hubIAM.GetAliases(account.client)
-	account.alias = response
+	account.response = response
 	return err
 }
 
 type UserCount struct {
 	client   apiclient.IAMListUsersAPIClient
-	userList hubIAM.UserList
+	response hubIAM.UserList
 }
 
 func (userCount *UserCount) Execute() error {
 	response, err := hubIAM.GetUserCount(userCount.client)
-	userCount.userList = response
+	userCount.response = response
 	return err
 }
 
 type UserIdentity struct {
 	client   apiclient.IAMGetUserAPIClient
-	identity hubIAM.User
+	response hubIAM.User
 }
 
 func (userIdentity *UserIdentity) Execute() error {
 	response, err := hubIAM.GetUserIdentity(userIdentity.client)
-	userIdentity.identity = response
+	userIdentity.response = response
 	return err
 }
 
--- handlers/aws/wrapper/sdk_test.go
+++ handlers/aws/wrapper/sdk_test.go
@@ -9,40 +9,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var client = new(iammock.MockClient)
+
 func TestAccountExecute(t *testing.T) {
 	assert := assert.New(t)
-	client := new(iammock.MockClient)
+
 	t.Parallel()
 	cases := []struct {
-		name           string
-		injectFunction string
-		expectedOutput interface{}
-		want           string
+		name            string
+		wrapperReceiver *SDK[*Account]
+		injectFunction  string
+		expectedOutput  interface{}
+		want            string
 	}{
 		{
-			name:           "Check ListAccountAliases via Mocking Framework",
-			injectFunction: "ListAccountAliases",
-			expectedOutput: &iam.ListAccountAliasesOutput{AccountAliases: []string{"aws-test-account-alias"}},
-			want:           "aws-test-account-alias",
+			name:            "Check ListAccountAliases via Mocking Framework",
+			wrapperReceiver: NewAccount(),
+			injectFunction:  "ListAccountAliases",
+			expectedOutput:  &iam.ListAccountAliasesOutput{AccountAliases: []string{"aws-test-account-alias"}},
+			want:            "aws-test-account-alias",
 		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
 			client.InjectFunctionMock(client, tt.injectFunction, tt.expectedOutput)
-			receiver := NewAccount(client)
+			receiver := tt.wrapperReceiver
 			err := receiver.Wrapper.Execute()
-			got := receiver.Wrapper.alias
+			got := receiver.Wrapper.response
 			assert.NoError(err, "expect no error, got %v", err)
 			assert.Equal(tt.want, got.List[0], "got GetAliases = %v, want = %v", got.List[0], tt.want)
 		})
 	}
 }
 
-func NewAccount(client *iammock.MockClient) *SDK[*Account] {
+func NewAccount() *SDK[*Account] {
 	receiver := New(&Account{
-		client: client,
-		alias:  hubIAM.Aliases{},
+		client:   client,
+		response: hubIAM.Aliases{},
 	})
 	return receiver
 }
