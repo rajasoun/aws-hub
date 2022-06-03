31d03d33050f908bc34db93df0265a27ecd7141d
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-03 11:22:49 +0000

    docs(automated-sequence-diagram): sequence digram generator for  mermaid markdown

---
+++ test/e2e/raja/e2e.md
@@ -0,0 +1,5 @@
+```mermaid 
+sequenceDiagram
+	actor User
+	User ->> main : aws-env go run main.go start 
+```
---
+++ test/e2e/raja/e2e_flow_test.go
@@ -0,0 +1,44 @@
+package raja
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func setUp() *os.File {
+	logFile, err := os.OpenFile("e2e.md", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	return logFile
+}
+
+func LogFlow(flow string) {
+	log.Println(flow)
+}
+
+func Flow(sender string, reciever string, message string) string {
+	flowDoc := fmt.Sprintf("%s ->> %s : %s ", sender, reciever, message)
+	return flowDoc
+}
+
+var startDoc = "```mermaid \nsequenceDiagram\n	actor User"
+var endDoc = "```"
+
+func TestE2E(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	fileLog := setUp()
+	log.SetOutput(fileLog)
+	log.SetFlags(0)
+	defer fileLog.Close()
+	LogFlow(startDoc)
+	flowDoc := Flow("User", "main", "aws-env go run main.go start")
+	LogFlow("\t" + flowDoc)
+	assert.Contains(flowDoc, "User", "")
+	LogFlow(endDoc)
+}
