f3e8d7e42f8a6a31ac05a92686a19153b77881b4
“ajit <ajithkumar.sinha@srsconsultinginc.com>
2022-06-06 12:32:21 +0000

    refactor(adding-a-function): it is taking the initialization
    BREAKING CHANGE:

--- test/e2e/ajith/e2e_flow.go
+++ test/e2e/ajith/e2e_flow.go
@@ -5,84 +5,19 @@ import (
 	"io"
 	"log"
 	"os"
+
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
+func CreateMarkdown()(*os.File,error){
+	name := "e2e.md"
+	fileOpetion  :=os.O_RDWR | os.O_CREATE
+	filePermission := 0666
+	logFile ,err :=os.OpenFile(name,fileOpetion,os.FileMode(filePermission))
+	if err !=nil {
 
-var (
-	DefaultFileName                = "e2e.md"
-	DefaultFileOptions int         = os.O_RDWR | os.O_CREATE
-	DefaultPermission  os.FileMode = 0666
-	DefaultFileOpener              = os.OpenFile
-)
-
-type Markdown struct {
-	fileName       string
-	fileOptions    int
-	filePermission os.FileMode
-	FileOpener     func(string, int, os.FileMode) (*os.File, error)
-}
-
-type FlowManager struct {
-	startDoc string
-	endDoc   string
-	markdown Markdown
-}
-
-//type FileOpener func(string, int, os.FileMode) (*os.File, error)
-
-type FlowAutomation interface {
-	CreateMarkdown() (*os.File, error)
-	Start(writer io.Writer)
-	End()
-}
-
-func NewFlowManager() *FlowManager {
-	markdown := Markdown{
-		fileName:       DefaultFileName,
-		fileOptions:    DefaultFileOptions,
-		filePermission: DefaultPermission,
-		FileOpener:     DefaultFileOpener,
-	}
-
-	flowManager := FlowManager{
-		startDoc: "```mermaid\n\nsequenceDiagram\n\tactor User",
-		endDoc:   "\n```",
-		markdown: markdown,
-	}
-	return &flowManager
-}
-
-//func (flowmanager *FlowManager) CreateMarkdown(fileOpener FileOpener) (*os.File, error) {
-func (fm *FlowManager) CreateMarkdown() (*os.File, error) {
-	//logFile, err := fileOpener(flowmanager.fileName, flowmanager.fileOptions, flowmanager.filePermission)
-	mk := fm.markdown
-	logFile, err := mk.FileOpener(mk.fileName, mk.fileOptions, mk.filePermission)
-	if err != nil {
-		log.Printf("Error Opening or Creating File %s Err = %v", mk.fileName, err)
-		return nil, err
+		log.Println("error in oepaning and creation %s err= %v",name,err)
+		return nil ,err
 	}
 	return logFile, nil
-}
 
-func (fm *FlowManager) Start(writer io.Writer) {
-	log.SetOutput(writer)
-	log.SetFlags(0)
-	log.Println(fm.startDoc)
-}
-
-func (fm *FlowManager) End() {
-	log.Println(fm.endDoc)
-}
-
-type Flow struct {
-	format    string
-	sender    string
-	receiver  string
-	direction string
-	message   string
-}
-
-func (flow *Flow) GetMermaidFlow() string {
-	flowDoc := fmt.Sprintf("%s\t%s %s %s : %s ",
-		flow.format, flow.sender, flow.direction, flow.receiver, flow.message)
-	return flowDoc
-}
+}func flowmanager()
