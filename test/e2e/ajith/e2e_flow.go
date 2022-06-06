87133367d855bcd3f6f9b33b584af70b17b1044e
“ajit <ajithkumar.sinha@srsconsultinginc.com>
2022-06-06 08:48:55 +0000

    adding e2e_test.go

---
+++ test/e2e/ajith/e2e_flow.go
@@ -0,0 +1,88 @@
+package ajith
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+var (
+	DefaultFileName                = "e2e.md"
+	DefaultFileOptions int         = os.O_RDWR | os.O_CREATE
+	DefaultPermission  os.FileMode = 0666
+	DefaultFileOpener              = os.OpenFile
+)
+
+type Markdown struct {
+	fileName       string
+	fileOptions    int
+	filePermission os.FileMode
+	FileOpener     func(string, int, os.FileMode) (*os.File, error)
+}
+
+type FlowManager struct {
+	startDoc string
+	endDoc   string
+	markdown Markdown
+}
+
+//type FileOpener func(string, int, os.FileMode) (*os.File, error)
+
+type FlowAutomation interface {
+	CreateMarkdown() (*os.File, error)
+	Start(writer io.Writer)
+	End()
+}
+
+func NewFlowManager() *FlowManager {
+	markdown := Markdown{
+		fileName:       DefaultFileName,
+		fileOptions:    DefaultFileOptions,
+		filePermission: DefaultPermission,
+		FileOpener:     DefaultFileOpener,
+	}
+
+	flowManager := FlowManager{
+		startDoc: "```mermaid\n\nsequenceDiagram\n\tactor User",
+		endDoc:   "\n```",
+		markdown: markdown,
+	}
+	return &flowManager
+}
+
+//func (flowmanager *FlowManager) CreateMarkdown(fileOpener FileOpener) (*os.File, error) {
+func (fm *FlowManager) CreateMarkdown() (*os.File, error) {
+	//logFile, err := fileOpener(flowmanager.fileName, flowmanager.fileOptions, flowmanager.filePermission)
+	mk := fm.markdown
+	logFile, err := mk.FileOpener(mk.fileName, mk.fileOptions, mk.filePermission)
+	if err != nil {
+		log.Printf("Error Opening or Creating File %s Err = %v", mk.fileName, err)
+		return nil, err
+	}
+	return logFile, nil
+}
+
+func (fm *FlowManager) Start(writer io.Writer) {
+	log.SetOutput(writer)
+	log.SetFlags(0)
+	log.Println(fm.startDoc)
+}
+
+func (fm *FlowManager) End() {
+	log.Println(fm.endDoc)
+}
+
+type Flow struct {
+	format    string
+	sender    string
+	receiver  string
+	direction string
+	message   string
+}
+
+func (flow *Flow) GetMermaidFlow() string {
+	flowDoc := fmt.Sprintf("%s\t%s %s %s : %s ",
+		flow.format, flow.sender, flow.direction, flow.receiver, flow.message)
+	return flowDoc
+}
