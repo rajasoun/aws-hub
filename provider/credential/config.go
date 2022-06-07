42b6f8b8f82d76a4899a91ddd78ae713e4978d30
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-07 15:02:22 +0000

    refactor(credital management tests): load default configuration

--- provider/credential/config.go
+++ provider/credential/config.go
@@ -2,6 +2,7 @@ package credential
 
 import (
 	"context"
+	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -9,9 +10,21 @@ import (
 
 var defaultRegion string = "us-east-1"
 
+type ConfigManager interface {
+	LoadDefaultConfig(ctx context.Context, optFns ...func(*config.LoadOptions) error) (cfg aws.Config, err error)
+}
+
+// Loads Default Configuration from files ~/.aws/config and ~/.aws/credential
+// Output:
+//     If successful, aws.Config struct & nil
+//     Otherwise, empty aws.Config and an error.
+//	   Ref: https://aws.github.io/aws-sdk-go-v2/docs/configuring-sdk/
 func LoadDefaultProfile() (aws.Config, error) {
 	context := context.TODO()
 	region := config.WithRegion(defaultRegion)
 	cfg, err := config.LoadDefaultConfig(context, region)
+	if err != nil {
+		log.Printf("failed to load default configuration, %v", err)
+	}
 	return cfg, err
 }
--- provider/credential/config_test.go
+++ provider/credential/config_test.go
@@ -9,11 +9,15 @@ import (
 func TestLoadDefaultProfile(t *testing.T) {
 	assert := assert.New(t)
 	t.Parallel()
-	t.Run("Check Loading Default Profile with Region", func(t *testing.T) {
+	t.Run("Check Load Default Profile with Region", func(t *testing.T) {
 		cfg, err := LoadDefaultProfile()
 		got := cfg.Region
 		want := "us-east-1"
 		assert.NoError(err, "LoadDefaultProfile() error = %v", err)
 		assert.Equal(got, want, "LoadDefaultProfile() = %v, want %v", got, want)
+	})
+	t.Run("Check Load Default Profile For Err", func(t *testing.T) {
+		_, err := LoadDefaultProfile()
+		assert.Error(err, "LoadDefaultProfile() error = %v", err)
 	})
 }
