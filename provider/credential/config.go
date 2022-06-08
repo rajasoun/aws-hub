f163a0af9b172357d77a9f2cf97b8d13e1c9339b
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-08 04:02:22 +0000

    refactor(credital management tests): load default configuration

--- provider/credential/config.go
+++ provider/credential/config.go
@@ -10,16 +10,18 @@ import (
 
 var defaultRegion string = "us-east-1"
 
-type ConfigManager interface {
+type ConfigLoader interface {
 	LoadDefaultConfig(ctx context.Context, optFns ...func(*config.LoadOptions) error) (cfg aws.Config, err error)
 }
+
+type CredentialLoader struct{}
 
 // Loads Default Configuration from files ~/.aws/config and ~/.aws/credential
 // Output:
 //     If successful, aws.Config struct & nil
 //     Otherwise, empty aws.Config and an error.
 //	   Ref: https://aws.github.io/aws-sdk-go-v2/docs/configuring-sdk/
-func LoadDefaultProfile() (aws.Config, error) {
+func (credLoader *CredentialLoader) LoadDefaultProfile() (aws.Config, error) {
 	context := context.TODO()
 	region := config.WithRegion(defaultRegion)
 	cfg, err := config.LoadDefaultConfig(context, region)
