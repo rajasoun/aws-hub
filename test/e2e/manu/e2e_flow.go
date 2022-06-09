94c60a9535da9dd0eb5435aaeb737e866cc4971f
“Manu <manu.vijaykumar@srsconsultinginc.com>
2022-06-09 10:15:37 +0000

    docs(adding-function-to-create-.md-file): need to write tdd for the same

---
+++ test/e2e/manu/e2e_flow.go
@@ -0,0 +1,32 @@
+package manu
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+/*var (
+	file string
+)*/
+
+func Createfile() {
+	file, err := os.Create("flow.md")
+	if err != nil {
+		fmt.Println("error creating file", err)
+	}
+	fmt.Println("File created successfully", file)
+	defer Writefile()
+}
+
+func Writefile() {
+	val := "hello, world\n"
+	data := []byte(val)
+
+	file := ioutil.WriteFile("flow.md", data, 0)
+
+		log.Printf("%v",file)
+
+
+}
