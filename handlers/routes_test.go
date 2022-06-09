3bba751e2a5c046f4cd1af48a38ad19605f55f3b
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-09 10:52:43 +0000

    refactoring(handlers): unit test modularization

--- handlers/routes_test.go
+++ handlers/routes_test.go
@@ -63,14 +63,16 @@ func TestRoutes(t *testing.T) {
 		name     string
 		endPoint string
 		mockIt   bool
+		wantErr  bool
 	}{
-		{"Check Health Check Handler", "/health", true},
-		{"Check Profile Handler", "/aws/profiles", true},
-		{"Check Profile Handler", "/aws/iam/users", true},
-		{"Check Profile Handler", "/aws/iam/account", true},
-		{"Check Profile Handler", "/aws/iam/alias", true},
-		{"Check Health Check Handler", "/health", false},
-		{"Check Profile Handler", "/aws/profiles", false},
+		{"Check Health Check Handler via Mock", "/health", true, false},
+		{"Check Profile Handler Via Mock", "/aws/profiles", true, false},
+		{"Check Profile Handler Via Mock", "/aws/iam/users", true, false},
+		{"Check Profile Handler Via Mock", "/aws/iam/account", true, false},
+		{"Check Profile Handler Via Mock", "/aws/iam/alias", true, false},
+		{"Check Health Check Handler Via Actual Call", "/health", false, false},
+		{"Check Profile Handler Via Actual Call", "/aws/profiles", false, false},
+		{"Check InValid Route Via Actual Call", "/aws/dummy", false, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -79,6 +81,10 @@ func TestRoutes(t *testing.T) {
 				response = getMockResponse(router, request)
 			} else {
 				response = executeRequest(router, request)
+			}
+			if tt.wantErr {
+				assert.Equal(http.StatusNotFound, response.Code, "http.StatusNotFound (404) response is expected")
+				return
 			}
 			assert.Equal(http.StatusOK, response.Code, "OK response is expected")
 		})
