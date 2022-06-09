d3588c7d356d2866fddcbad2cd1b11878054e0ab
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-09 09:17:21 +0000

    refactoring(handlers): unit test modularization

--- handlers/routes_test.go
+++ handlers/routes_test.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -45,4 +47,38 @@ func TestAPIRoutes(t *testing.T) {
 			assert.Equal(tt.endPoint, got, "got = %v, want = %v", got, tt.endPoint)
 		})
 	}
+}
+
+func TestRoutes(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	var request *http.Request
+	var response *httptest.ResponseRecorder
+
+	handler := NewDefaultAWSHandler(false)
+	router := handler.SetUpRoutes()
+
+	tests := []struct {
+		name     string
+		endPoint string
+	}{
+		{"Check Health Check Handler", "/health"},
+		{"Check Profile Handler", "/aws/profiles"},
+	}
+	//Note Roues Making AWS Calls are not included as they are not unit Tests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request, _ = http.NewRequest("GET", tt.endPoint, nil)
+			response = executeRequest(router, request)
+			assert.Equal(http.StatusOK, response.Code, "OK response is expected")
+		})
+	}
+}
+
+// IssueTestRequest executes an HTTP request described by rt, to a
+// specified REST router.  It returns the HTTP response to the request.
+func executeRequest(router *mux.Router, req *http.Request) *httptest.ResponseRecorder {
+	responseRecorder := httptest.NewRecorder()
+	router.ServeHTTP(responseRecorder, req)
+	return responseRecorder
 }
