5976f0807545552abef02e8f96ee95e91b9df6e5
Rohini Gorige <rohini.gorige@srsconsultinginc.com>
2022-06-10 06:55:25 +0000

    style(removing-the-unwanted-test-format): in e2e removing t.run for test cases

--- test/e2e/rohini/e2e_flow_test.go
+++ test/e2e/rohini/e2e_flow_test.go
@@ -1,7 +1,6 @@
 package rohini
 
 import (
-	"log"
 	"testing"
 )
 
@@ -12,37 +11,6 @@ func TestE2E(t *testing.T) {
 	defer flowLog.Close()
 
 	Start(flowLog)
-	flow := Flow{}
-	t.Run("User To main", func(t *testing.T) {
-		flow.format = "\t"
-		flow.sender = "User"
-		flow.direction = " ->> "
-		flow.receiver = "main"
-		flow.message = "aws-env go run main.go start"
-		flowDoc := flow.GetMermaidFlow()
-		log.Println(flowDoc)
-	})
+	///flow := Flow{}
 
-	t.Run("main to app.hub", func(t *testing.T) {
-		flow.format = "\t"
-		flow.sender = "main"
-		flow.direction = " ->> "
-		flow.receiver = "app.hub"
-		flow.message = "Execute()"
-		flowDoc := flow.GetMermaidFlow()
-		log.Println(flowDoc)
-
-	})
-
-	t.Run("main to app.hub", func(t *testing.T) {
-		flow.format = "\t"
-		flow.sender = "app.hub"
-		flow.direction = " ->> "
-		flow.receiver = "urfave.cli"
-		flow.message = "app.cli.Run(args)"
-		flowDoc := flow.GetMermaidFlow()
-		log.Println(flowDoc)
-	})
-
-	End()
 }
