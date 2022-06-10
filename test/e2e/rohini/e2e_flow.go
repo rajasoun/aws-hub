af1035e325d0314535530d5437ec4f81c997196e
Rohini Gorige <rohini.gorige@srsconsultinginc.com>
2022-06-10 04:46:51 +0000

    style(writtenfunction-which-is-failing-to-open-sequence-flow-diagram): neeed to get a sequence flow diagram in preview chart

---
+++ test/e2e/rohini/e2e_flow.go
@@ -0,0 +1,49 @@
+package rohini
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+)
+
+var startDoc string = "```mermaid\n\nsequenceDiagram\n\tactor User"
+var endDoc string = "\n```"
+
+func CreateMarkdown() (*os.File, error) {
+	DefaultFileName := "e2e.md"
+	DefaultFileOptions := os.O_RDWR | os.O_CREATE
+	DefaultPermission := 0666
+
+	logFile, err := os.OpenFile(DefaultFileName, DefaultFileOptions, fs.FileMode(DefaultPermission))
+	if err != nil {
+		log.Printf("Error Opening or Creating a file %s Err = %v", DefaultFileName, err)
+		return nil, err
+	}
+
+	return logFile, nil
+
+}
+func Start(writer io.Writer) {
+	log.SetOutput(writer)
+	log.SetFlags(0)
+	log.Println(startDoc)
+}
+func End() {
+	log.Println(endDoc)
+}
+
+type Flow struct {
+	format    string
+	sender    string
+	receiver  string
+	direction string
+	message   string
+}
+
+func (flow *Flow) GetMermaidFlow() string {
+	flowDoc := fmt.Sprintf("%s\t%s %s %s : %s",
+		flow.format, flow.sender, flow.receiver, flow.direction, flow.message)
+	return flowDoc
+}
