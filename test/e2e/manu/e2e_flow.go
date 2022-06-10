b33e18ec81fe0128298370de7aca386ffd67962c
“Manu <manu.vijaykumar@srsconsultinginc.com>
2022-06-10 10:45:13 +0000

    style(duplicated-the-code): trying to refactor

--- test/e2e/manu/e2e_flow.go
+++ test/e2e/manu/e2e_flow.go
@@ -2,14 +2,94 @@ package manu
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 )
 
-/*var (
-	file string
-)*/
+var (
+	FileName                = "e2e.md"
+	FileOptions int         = os.O_RDWR | os.O_CREATE
+	Permission  os.FileMode = 0666
+	FileOpener              = os.OpenFile
+)
+
+type Markdown struct {
+	fileName       string
+	fileOptions    int
+	filePermission os.FileMode
+	FileOpener     func(string, int, os.FileMode) (*os.File, error)
+}
+
+type FlowManager struct {
+	startDoc string
+	endDoc   string
+	markdown Markdown
+}
+
+
+
+type FlowAutomation interface {
+	CreateMarkdown() (*os.File, error)
+	Start(writer io.Writer)
+	End()
+}
+
+func NewFlowManager() *FlowManager {
+	markdown := Markdown{
+		fileName:       FileName,
+		fileOptions:    FileOptions,
+		filePermission: Permission,
+		FileOpener:     FileOpener,
+	}
+
+	flowManager := FlowManager{
+		startDoc: "```mermaid\n\nsequenceDiagram\n\tactor User",
+		endDoc:   "\n```",
+		markdown: markdown,
+	}
+	return &flowManager
+}
+
+func (fm *FlowManager) CreateMarkdown() (*os.File, error) {
+	mk := fm.markdown
+	logFile, err := mk.FileOpener(mk.fileName, mk.fileOptions, mk.filePermission)
+	if err != nil {
+		log.Printf("Error Opening or Creating File %s Err = %v", mk.fileName, err)
+		return nil, err
+	}
+	return logFile, nil
+}
+
+func (fm *FlowManager) Start(writer io.Writer) {
+	log.SetOutput(writer)
+	log.SetFlags(0)
+	log.Println(fm.startDoc)
+}
+
+func (fm *FlowManager) End() {
+	log.Println(fm.endDoc)
+}
+
+type Flow struct {
+	format    string
+	sender    string
+	receiver  string
+	direction string
+	message   string
+}
+
+func (flow *Flow) GetMermaidFlow() string {
+	flowDoc := fmt.Sprintf("%s\t%s %s %s : %s ",
+		flow.format, flow.sender, flow.direction, flow.receiver, flow.message)
+	return flowDoc
+}
+
+/*
+var (
+	Start string = "```mermaid\n\nsequenceDiagram\n\tactor User"
+	End string =   " \n```"
+)
 
 func Createfile() {
 	file, err := os.Create("flow.md")
@@ -18,14 +98,17 @@ func Createfile() {
 	}
 	fmt.Println("File created successfully", file)
 	defer Writefile()
-}
+	}
+
 
 func Writefile() {
-	val := "hello, world\n"
-	data := []byte(val)
 
-	file := ioutil.WriteFile("flow.md", data, 0)
+	data1 := []byte(Start,)
+	//data2 := []byte (End)
 
+	file := ioutil.WriteFile("flow.md", data1, 0)
 	log.Printf("%v", file)
 
 }
+
+*/
--- test/e2e/manu/e2e_flow_test.go
+++ test/e2e/manu/e2e_flow_test.go
@@ -1,1 +1,79 @@
 package manu
+
+import (
+	"log"
+	"testing"
+)
+
+func TestE2E(t *testing.T) {
+	t.Parallel()
+	var flowByCoding FlowAutomation = NewFlowManager()
+
+	flowLog, _ := flowByCoding.CreateMarkdown()
+	defer flowLog.Close()
+	flows := []struct {
+		name string
+		flow Flow
+	}{
+		{
+			name: "User To main",
+			flow: Flow{
+				format:    "\t",
+				sender:    "User",
+				direction: " ->> ",
+				receiver:  "main",
+				message:   "aws-env go run main.go start",
+			},
+		},
+		{
+			name: "main to app.hub",
+			flow: Flow{
+				format:    "\t",
+				sender:    "main",
+				direction: " ->> ",
+				receiver:  "app.hub",
+				message:   "Execute()",
+			},
+		},
+		{
+			name: "urfave.cli to start command",
+			flow: Flow{
+				format:    "\t",
+				sender:    "urfave.cli",
+				direction: " ->> ",
+				receiver:  "app.config.cmd.startCmd",
+				message:   "StartCommand(appCtx *cli.Context)",
+			},
+		},
+		{
+			name: "start command to server start",
+			flow: Flow{
+				format:    "\t",
+				sender:    "app.config.cmd.startCmd",
+				direction: " ->> ",
+				receiver:  "app.server",
+				message:   "Start(port, enableShutdown)",
+			},
+		},
+	}
+	flowByCoding.Start(flowLog)
+	for _, tt := range flows {
+		t.Run(tt.name, func(t *testing.T) {
+			log.Println(tt.flow.GetMermaidFlow())
+		})
+	}
+	log.Println("\t\tNote right of app.server: Server Started!")
+	flowByCoding.End()
+}
+
+/*
+func TestE2E(t *testing.T) {
+
+	got := Createfile(file)
+	want := "flow.md"
+
+	if got != want {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+}
+*/
