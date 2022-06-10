6a0d05564a6641398fdebf92601acdb3ff8b5ee9
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-10 08:28:21 +0000

    tests(tdd examples): TDD for Handlers

--- test/examples/httptest/product.go
+++ test/examples/httptest/product.go
@@ -2,14 +2,17 @@ package spike
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var storeDB []Product
 
 // Product represents a product in the store
 type Product struct {
-	Title          string  `json:"title,omitempty"`
+	Name           string  `json:"title,omitempty"`
 	Price          float64 `json:"price,omitempty"`
 	InventoryCount int     `json:"inventory_count,omitempty"`
 }
@@ -24,4 +27,21 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(products)
 
+}
+
+// GetProduct returns the product with the specified title
+func GetProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["title"]
+	for _, v := range storeDB {
+		if v.Name == name {
+			log.Printf("SAME -> v.Title = %v, title = %v", v.Name, name)
+			json.NewEncoder(w).Encode(v)
+			return
+		}
+	}
+	//if no product is found
+	log.Printf("Not Found Product for Name = %v", name)
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode("Not Found")
 }
--- test/examples/httptest/product_test.go
+++ test/examples/httptest/product_test.go
@@ -5,33 +5,64 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
 func setUpStoreDB() {
-	paste := Product{Title: "Paste", Price: 10, InventoryCount: 10}
-	soap := Product{Title: "Soap", Price: 20, InventoryCount: 5}
+	paste := Product{Name: "Paste", Price: 10, InventoryCount: 10}
+	soap := Product{Name: "Soap", Price: 20, InventoryCount: 5}
 	storeDB = append(storeDB, paste)
 	storeDB = append(storeDB, soap)
 }
 
-func TestGetProductsHandler(t *testing.T) {
+func TestGetProducts(t *testing.T) {
 	assert := assert.New(t)
 	t.Parallel()
 	setUpStoreDB()
 	t.Run("Check Get Products", func(t *testing.T) {
-		request, err := http.NewRequest("GET", "/test", nil)
-		assert.NoError(err, "http.NewRequest() = %v ", err)
-		responseRecorder := httptest.NewRecorder()
-
-		// Set Handler
-		handler := http.HandlerFunc(GetProducts)
-		// ServeHTTP calls
-		handler.ServeHTTP(responseRecorder, request)
-
+		responseRecorder := executeHandler(GetProducts, map[string]string{})
 		got := responseRecorder.Code
 		want := http.StatusOK
 		assert.Equal(got, want, "handler returned wrong status code: got %v want %v", got, want)
 	})
+}
 
+func TestGetProduct(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	setUpStoreDB()
+	tests := []struct {
+		name    string
+		want    int
+		muxVars map[string]string
+	}{
+		{
+			name:    "GetProduct: valid test case",
+			muxVars: map[string]string{"title": "Soap"},
+			want:    http.StatusOK,
+		},
+		{
+			name:    "GetProduct: invalid test case",
+			muxVars: map[string]string{"title": "invalid_value"},
+			want:    http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("Check Get Products", func(t *testing.T) {
+			responseRecorder := executeHandler(GetProduct, tt.muxVars)
+			got := responseRecorder.Code
+			assert.Equal(tt.want, got, "handler returned wrong status code: got %v want %v", got, tt.want)
+		})
+	}
+}
+
+func executeHandler(handlerName func(w http.ResponseWriter, r *http.Request),
+	muxRequestVars map[string]string) *httptest.ResponseRecorder {
+	request, _ := http.NewRequest("GET", "/test", nil)
+	request = mux.SetURLVars(request, muxRequestVars)
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlerName)
+	handler.ServeHTTP(responseRecorder, request)
+	return responseRecorder
 }
