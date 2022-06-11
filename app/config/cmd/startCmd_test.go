4e32adf8b53d08e02ab2363fc238f811c04589f5
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-11 08:37:29 +0000

    refactor(tests) : increase coverage

--- app/config/cmd/startCmd_test.go
+++ app/config/cmd/startCmd_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"flag"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,10 +13,23 @@ func TestStartCmd(t *testing.T) {
 	assert := assert.New(t)
 	t.Parallel()
 
-	t.Run("Check Start Server with Mock Start Function", func(t *testing.T) {
+	t.Run("Check Start Server With Empty Context", func(t *testing.T) {
 		cmdhandler := CmdHandler{}
 		cmdhandler.EnableShutdDown = true
 		err := cmdhandler.StartCommand(&cli.Context{})
+		assert.NoError(err, "err = %v ", err)
+	})
+	t.Run("Check Start Server With Custom Port Zero", func(t *testing.T) {
+		mockApp := &cli.App{Writer: ioutil.Discard}
+		set := flag.NewFlagSet("test", 0)
+		_ = set.Parse([]string{"start", "--port", "0"})
+		cCtx := cli.NewContext(mockApp, set, nil)
+
+		cmdhandler := CmdHandler{}
+		cmdhandler.EnableShutdDown = true
+		startCommand := GetCommand(cmdhandler.StartCommand)
+
+		err := startCommand.Run(cCtx)
 		assert.NoError(err, "err = %v ", err)
 	})
 }
