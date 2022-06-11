077b01efeffcfe22b36a33ba2c75e073b9c2f421
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-11 09:30:03 +0000

    refactor(tests) : increase coverage

--- app/config/cmd/startCmd_test.go
+++ app/config/cmd/startCmd_test.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"flag"
 	"io/ioutil"
+	"strconv"
 	"testing"
 
+	"github.com/rajasoun/aws-hub/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/urfave/cli/v2"
 )
@@ -13,23 +15,17 @@ func TestStartCmd(t *testing.T) {
 	assert := assert.New(t)
 	t.Parallel()
 
-	t.Run("Check Start Server With Empty Context", func(t *testing.T) {
-		cmdhandler := CmdHandler{}
-		cmdhandler.EnableShutdDown = true
-		err := cmdhandler.StartCommand(&cli.Context{})
-		assert.NoError(err, "err = %v ", err)
-	})
-	t.Run("Check Start Server With Custom Port Zero", func(t *testing.T) {
+	t.Run("Check Start Command With Custom Port Zero", func(t *testing.T) {
 		mockApp := &cli.App{Writer: ioutil.Discard}
 		set := flag.NewFlagSet("test", 0)
-		_ = set.Parse([]string{"start", "--port", "0"})
-		cCtx := cli.NewContext(mockApp, set, nil)
-
+		port, _ := test.GetFreePort()
+		portString := strconv.Itoa(port)
+		_ = set.Parse([]string{"start", "--port", portString})
+		context := cli.NewContext(mockApp, set, nil)
 		cmdhandler := CmdHandler{}
 		cmdhandler.EnableShutdDown = true
 		startCommand := GetCommand(cmdhandler.StartCommand)
-
-		err := startCommand.Run(cCtx)
+		err := startCommand.Run(context)
 		assert.NoError(err, "err = %v ", err)
 	})
 }
--- test/init.go
+++ test/init.go
@@ -3,6 +3,7 @@ package test
 import (
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -33,4 +34,19 @@ func ExecuteHandler(handlerName func(w http.ResponseWriter, r *http.Request),
 	handler := http.HandlerFunc(handlerName)
 	handler.ServeHTTP(responseRecorder, request)
 	return responseRecorder
+}
+
+// GetFreePort asks the kernel for a free open port that is ready to use.
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
