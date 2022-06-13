6bb1678dc978243b20f72ec6852128b34cbc82fb
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-13 11:09:14 +0000

    build(git) : fix n commits behind

---
+++ build/git/sync.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function fix_n_commits_behind(){
+    main_branch = $1
+    dev_branch = $2
+    git checkout $main_branch
+    git pull origin $main_branch --rebase
+    git checkout $dev_branch
+    git merge $main_branch
+    # dev_branch is in sync with local main_branch
+    git push origin dev_branch
+}
+
+fix_n_commits_behind "main" "develop"
+fix_n_commits_behind "develop" "integration_branch"
