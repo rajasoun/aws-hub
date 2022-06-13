5701af63b17840da000929a8c623349d3baa7c02
“ajit <ajithkumar.sinha@srsconsultinginc.com>
2022-06-13 10:19:03 +0000

    refactor(with-structs): adding tdd with structs

--- test/examples/interface/new1.go
+++ test/examples/interface/new1.go
@@ -1,5 +1,10 @@
 package spike
 
-func Perimeter(wi float64, he float64) float64 {
-	return 2 * (wi + he)
+type perimeter struct {
+	wi float64
+	he float64
+}
+
+func (p *perimeter) Perimeter() float64 {
+	return 2 * (p.wi + p.he)
 }
--- test/examples/interface/new1_test.go
+++ test/examples/interface/new1_test.go
@@ -1,33 +1,35 @@
 package spike
 
-//ajit
-
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	type args struct {
+func Test_perimeter_Perimeter(t *testing.T) {
+	type fields struct {
 		wi float64
 		he float64
 	}
 	tests := []struct {
-		name string
-		args args
-		want float64
+		name   string
+		fields fields
+		want   float64
 	}{
 		// TODO: Add test cases.
 		{
 			name: "perimeter",
-			args: args{
-				wi: 1,
-				he: 1,
+			fields: fields{
+				wi: 12,
+				he: 12,
 			},
-			want: 4.0,
+			want: 48,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Perimeter(tt.args.wi, tt.args.he); got != tt.want {
-				t.Errorf("Perimeter() = %v, want %v", got, tt.want)
+			p := &perimeter{
+				wi: tt.fields.wi,
+				he: tt.fields.he,
+			}
+			if got := p.Perimeter(); got != tt.want {
+				t.Errorf("perimeter.Perimeter() = %v, want %v", got, tt.want)
 			}
 		})
 	}
