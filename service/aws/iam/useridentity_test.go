de6e004c8f5971d6cd3a84abe2f6b18441867f20
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-14 09:05:52 +0000

    refactor(tests) : for speed

--- service/aws/iam/useridentity_test.go
+++ service/aws/iam/useridentity_test.go
@@ -73,19 +73,23 @@ func TestGetUserIdentityViaMockFramework(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			client := new(MockUserIdentity)
-			expectedOutput := &iam.GetUserOutput{User: tt.input}
+			expectedOutput := &iam.GetUserOutput{
+				User: tt.input,
+			}
 			// Inject Mock Function to be Called along with Resturn values as Parameter
 			client.
 				On("GetUser", mock.Anything, mock.Anything, mock.Anything).
 				Return(expectedOutput, tt.wantErr)
 			got, err := GetUserIdentity(client)
-			assert.Equal(tt.want, got.Username, "GetUserIdentity().Username = %v, want = %v", got.Username, tt.name)
 			if tt.wantErr != nil {
 				assert.Error(err, " GetUserIdentity() %v", err)
+				assert.Empty(got.Username, "GetUserIdentity().Username = %v", got.Username)
 				return
 			}
 			assert.NoError(err, " GetUserIdentity() %v", err)
+			assert.Equal(tt.want, got.Username, "GetUserIdentity().Username = %v, want = %v", got.Username, tt.name)
 		})
+
 	}
 }
 
