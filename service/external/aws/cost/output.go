90fd392a3601f1f539b36e1cc2d478fba6cb9a1e
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-18 13:01:13 +0000

    feat (cost explorer api) :  GetCostAndUsage feature

--- service/external/aws/cost/output.go
+++ service/external/aws/cost/output.go
@@ -1,23 +1,28 @@
 package cost
 
 import (
+	"log"
 	"sort"
 	"strconv"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
 	"github.com/aws/aws-sdk-go-v2/service/costexplorer/types"
-	awsModel "github.com/rajasoun/aws-hub/service/external/aws/cost/model"
+	model "github.com/rajasoun/aws-hub/service/external/aws/cost/model"
 )
 
 const bitSize = 64
 
-func groupByMetrics(res types.ResultByTime) (string, []awsModel.Group) {
+func groupByMetrics(res types.ResultByTime) (string, []model.Group) {
 	unit := "USD"
-	groups := make([]awsModel.Group, 0)
+	groups := make([]model.Group, 0)
+	if len(res.Groups) == 0 {
+		log.Println("Got Empty Groups")
+		return unit, groups
+	}
 	for _, group := range res.Groups {
 		amount, _ := strconv.ParseFloat(*group.Metrics["BlendedCost"].Amount, bitSize)
-		groups = append(groups, awsModel.Group{
+		groups = append(groups, model.Group{
 			Key:    group.Keys[0],
 			Amount: amount,
 		})
@@ -26,9 +31,9 @@ func groupByMetrics(res types.ResultByTime) (string, []awsModel.Group) {
 	return unit, groups
 }
 
-func appendToList(costs []awsModel.Cost, start time.Time,
-	end time.Time, unit string, groups []awsModel.Group) []awsModel.Cost {
-	costs = append(costs, awsModel.Cost{
+func appendToList(costs []model.Cost, start time.Time,
+	end time.Time, unit string, groups []model.Group) []model.Cost {
+	costs = append(costs, model.Cost{
 		Start:  start,
 		End:    end,
 		Unit:   unit,
@@ -37,8 +42,13 @@ func appendToList(costs []awsModel.Cost, start time.Time,
 	return costs
 }
 
-func costHistory(result *costexplorer.GetCostAndUsageOutput) []awsModel.Cost {
-	costList := make([]awsModel.Cost, 0)
+func costHistory(result *costexplorer.GetCostAndUsageOutput) []model.Cost {
+	costList := make([]model.Cost, 0)
+	if len(result.ResultsByTime) == 0 {
+		log.Println("Got Empty result.ResultsByTime")
+		return costList
+	}
+
 	for _, res := range result.ResultsByTime {
 		unit, groups := groupByMetrics(res)
 
@@ -54,7 +64,7 @@ func costHistory(result *costexplorer.GetCostAndUsageOutput) []awsModel.Cost {
 	return costList
 }
 
-func currentBill(costHistory []awsModel.Cost) float64 {
+func currentBill(costHistory []model.Cost) float64 {
 	var currentBill float64
 	for _, group := range costHistory[len(costHistory)-1].Groups {
 		currentBill += group.Amount
@@ -62,8 +72,12 @@ func currentBill(costHistory []awsModel.Cost) float64 {
 	return currentBill
 }
 
-func GetCostHistoryAndBill(result *costexplorer.GetCostAndUsageOutput) ([]awsModel.Cost, float64) {
+func GetCostHistoryAndBill(result *costexplorer.GetCostAndUsageOutput) ([]model.Cost, float64) {
 	costHistory := costHistory(result)
+	if len(costHistory) == 0 {
+		log.Println("Cost History is Empty")
+		return []model.Cost{}, 0
+	}
 	currentBill := currentBill(costHistory)
 	return costHistory, currentBill
 }
--- service/external/aws/cost/output_test.go
+++ service/external/aws/cost/output_test.go
@@ -2,8 +2,10 @@ package cost
 
 import (
 	"testing"
+	"time"
 
 	awsutil "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
 	"github.com/aws/aws-sdk-go-v2/service/costexplorer/types"
 	model "github.com/rajasoun/aws-hub/service/external/aws/cost/model"
 	"github.com/stretchr/testify/assert"
@@ -54,6 +56,146 @@ func TestGroupByMetrics(t *testing.T) {
 			got, got1 := groupByMetrics(tt.args.res)
 			assert.Equal(tt.want, got, "groupByMetrics() got = %v, want %v", got, tt.want)
 			assert.Equal(tt.want1, got1, "groupByMetrics() got = %v, want %v", got1, tt.want1)
+		})
+	}
+}
+
+func TestAppendToList(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	type args struct {
+		costs  []model.Cost
+		start  time.Time
+		end    time.Time
+		unit   string
+		groups []model.Group
+	}
+	tests := []struct {
+		name string
+		args args
+		want []model.Cost
+	}{
+		{
+			name: "Check AppendToList with Empty Cost",
+			args: args{
+				costs:  make([]model.Cost, 0),
+				start:  time.Time{},
+				end:    time.Time{},
+				unit:   "USD",
+				groups: []model.Group{},
+			},
+			want: []model.Cost{
+				{
+					Start:  time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+					End:    time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+					Unit:   "USD",
+					Groups: []model.Group{},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := appendToList(tt.args.costs, tt.args.start, tt.args.end, tt.args.unit, tt.args.groups)
+			assert.Equal(tt.want, got, "appendToList() = %v, want %v", got, tt.want)
+			assert.Equal(1, len(got), "appendToList() = %v, want %v", len(got), 1)
+		})
+	}
+}
+
+func TestCostHistory(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	type args struct {
+		result *costexplorer.GetCostAndUsageOutput
+	}
+	tests := []struct {
+		name string
+		args args
+		want []model.Cost
+	}{
+		{
+			name: "Check CostHistory with Empty result",
+			args: args{
+				result: &costexplorer.GetCostAndUsageOutput{},
+			},
+			want: []model.Cost{},
+		},
+		{
+			name: "Check CostHistory with Empty result",
+			args: args{
+				result: &costexplorer.GetCostAndUsageOutput{
+					ResultsByTime: []types.ResultByTime{},
+				},
+			},
+			want: []model.Cost{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := costHistory(tt.args.result)
+			assert.Equal(tt.want, got, "costHistory() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+func TestCurrentBill(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	type args struct {
+		costHistory []model.Cost
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "Check Current Bill",
+			args: args{
+				costHistory: []model.Cost{
+					{
+						Unit:   "USD",
+						Groups: []model.Group{{Key: "Total", Amount: 0}},
+					}},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := currentBill(tt.args.costHistory)
+			assert.Equal(tt.want, got, "currentBill() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+func TestGetCostHistoryAndBill(t *testing.T) {
+	assert := assert.New(t)
+	t.Parallel()
+	type args struct {
+		result *costexplorer.GetCostAndUsageOutput
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  []model.Cost
+		want1 float64
+	}{
+		{
+			name: "Check GetCostHistoryAndBill",
+			args: args{
+				result: &costexplorer.GetCostAndUsageOutput{},
+			},
+			want:  []model.Cost{},
+			want1: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetCostHistoryAndBill(tt.args.result)
+			assert.Equal(tt.want, got, "GetCostHistoryAndBill() got = %v, want %v", got, tt.want)
+			assert.Equal(tt.want1, got1, "GetCostHistoryAndBill() got = %v, want %v", got1, tt.want1)
 		})
 	}
 }
