2559d5306aec7d2c47f746417d1c463817a0def9
Raja Soundaramourty <rajasoun@cisco.com>
2022-06-20 09:41:33 +0000

    feat (cost explorer api) :  GetCostAndUsage feature

--- test/integration/iam_test.go
+++ test/integration/iam_test.go
@@ -32,28 +32,30 @@ func TestIAM(t *testing.T) {
 	t.Run("Check Account Alias", func(t *testing.T) {
 		got, err := hubiam.GetAliases(client)
 		assert.NoError(err, "GetAliases() = %v", err)
-		file, _ := ioutil.ReadFile("testdata/iam/alias.json")
 		want := hubiam.Aliases{}
-		json.Unmarshal(file, &want)
+		loadTestData("testdata/iam/alias.json", &want)
 		assert.Equal(want, got, "GetAliases() = %v, want = %v", got, want)
 	})
 	t.Run("Check Users Count", func(t *testing.T) {
 		got, err := hubiam.GetUserCount(client)
 		assert.NoError(err, "GetUserCount() = %v", err)
 		want := hubiam.UserList{}
-		file, _ := ioutil.ReadFile("testdata/iam/users.json")
-		json.Unmarshal(file, &want)
+		loadTestData("testdata/iam/users.json", &want)
 		assert.Equal(want, got, "GetUserCount() = %v, want = %v", got, want)
 	})
 	t.Run("Check User Identity", func(t *testing.T) {
 		got, err := hubiam.GetUserIdentity(client)
 		assert.NoError(err, "GetUserIdentity() = %v", err)
 		want := hubiam.User{}
-		file, _ := ioutil.ReadFile("testdata/iam/identity.json")
-		json.Unmarshal(file, &want)
+		loadTestData("testdata/iam/identity.json", &want)
 		assert.Equal(want.ARN, got.ARN, "GetUserIdentity() ARN = %v, want = %v", got.ARN, want.ARN)
 		assert.Equal(want.CreateDate, got.CreateDate, "GetUserIdentity() CreateDate = %v, want = %v", got.CreateDate, want.CreateDate)
 		assert.Equal(want.UserID, got.UserID, "GetUserIdentity() UserID = %v, want = %v", got.UserID, want.UserID)
 		assert.Equal(want.Username, got.Username, "GetUserIdentity() Username = %v, want = %v", got.Username, want.Username)
 	})
+}
+
+func loadTestData(filePath string, ds interface{}) {
+	file, _ := ioutil.ReadFile(filePath)
+	json.Unmarshal(file, &ds)
 }
